dd45f68171f8c90237bffd2d3fd36124b5baad6d
Daniele <daniele.ricotta@gmail.com>
2021-08-31 08:28:07 +0200

    highlight interest value

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ if (checkdata()) {
    var year = new Date().getFullYear()+parseInt(years);
    document.getElementById("result").innerHTML = "<p> If you deposit <mark>" + principal + "</mark>," + "<br />" +
                                                  "at an interest rate of <mark>" + rate + "</mark>%." + "<br />" +
-                                                 "You will receive an amount of " + interest + "," + "<br />" +
+                                                 "You will receive an amount of <mark>" + interest + "</mark>," + "<br />" +
                                                  "in the year <mark>" + year + "</mark>"
 }                                                         
     
